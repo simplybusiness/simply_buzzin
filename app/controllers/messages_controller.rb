4b120426344ccc6583a4cce9e5d231de0362a729
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-03-04 13:31:18 +0000

    added template file

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,8 @@ class MessagesController < ApplicationController
   end
 
   def create
-
     message_params = params['message']
-    message = "<html><head><meta charset='utf-8'></head><body><p>#{message_params['icon']}</p><p>#{message_params['title']}</p><p>#{message_params['header']}</p><p>#{message_params['body']}</p></body></html>"
+    message = render_to_string :partial => "message_template", :locals => {:message_params => message_params}
 
     little_printer_client = LittlePrinterClient.new(message_params['printer'])
     little_printer_client.message(message)
