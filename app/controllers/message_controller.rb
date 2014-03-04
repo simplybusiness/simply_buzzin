cb31087efcbda9e6dc3443f3751ba9eb62456d08
Angus Mark <ngsmrk@gmail.com>
2014-03-04 11:01:42 +0000

    Added message controller

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,10 @@
+class MessageController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 SimplyBuzzin::Application.routes.draw do
+
+  root :to => 'message#index'
+
+  get "message/index"
+
+  get "message/new"
+
+  get "message/create"
+
 end
