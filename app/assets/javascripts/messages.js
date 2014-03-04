5999273b76b3c420fb2533afdf6f191a5be97991
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-03-04 15:39:42 +0000

    new view

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,3 +1,21 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+ $(document).ready(function() {
+   $('.edit').editable(function(value, settings) {
+     element_id = this.id.replace("inplace-", "")
+     $('#message_'+element_id).val(value)
+     console.log
+     return value
+   }, {
+     cssclass : 'editable-input'
+   });
+
+   $('.edit_area').editable(function(value, settings) {
+    element_id = this.id.replace("inplace-", "")
+    $('#message_'+element_id).val(value)
+    return value
+   }, {
+     cssclass : 'editable-input',
+     type: 'textarea',
+     submit: 'OK',
+     cssclass : 'editable-area'
+   });
+ });
