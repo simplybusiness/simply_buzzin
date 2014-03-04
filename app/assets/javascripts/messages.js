2be03b8e347ff93fa4ae562fd7669ae344164748
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-03-04 16:36:09 +0000

    inplace editing

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,7 @@
  $(document).ready(function() {
    $('.edit').editable(function(value, settings) {
      element_id = this.id.replace("inplace-", "")
-     $('#message_'+element_id).val(value)
+     $('#message_'+element_id).val($.trim(value))
      console.log
      return value
    }, {
@@ -10,7 +10,7 @@
 
    $('.edit_area').editable(function(value, settings) {
     element_id = this.id.replace("inplace-", "")
-    $('#message_'+element_id).val(value)
+    $('#message_'+element_id).val($.trim(value))
     return value
    }, {
      cssclass : 'editable-input',
@@ -18,4 +18,24 @@
      submit: 'OK',
      cssclass : 'editable-area'
    });
+
+    // and the formula is:
+    var random_image = function() {
+      var max = 7;
+      var faces = ['boy', 'girl', 'jason'];
+      var random_number = Math.floor(Math.random() * (max-1)) + 1;
+      var face = faces[Math.floor(Math.random() * (faces.length - 1)) + 1];
+
+      return face+'_face_0'+random_number+'.png';
+    }
+
+    var start_image = random_image();
+    $('img.face-image').attr('src', 'images/' + start_image);
+    $('#message_icon').val(start_image);
+
+    $('.refresh-link').click(function () {
+      image_name = random_image();
+      $('img.face-image').attr('src', 'images/' + image_name);
+      $('#message_icon').val(image_name);
+    });
  });
