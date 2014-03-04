ab13df8e664e2140f7b5d24da7e688e904fdddd6
Angus Mark <ngsmrk@gmail.com>
2014-03-04 10:41:31 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplyBuzzin::Application.config.secret_token = '440e5cc745a2843f43cd4e0105fe20a18acf1aa3ef158bf926e4c6dec9379c63165d106ae4ee792ca194e933773b2e798cd4029303e98e1420f1bd125d309875'
