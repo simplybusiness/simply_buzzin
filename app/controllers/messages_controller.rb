de0c99025d04b7c499fdbc0d558d910a6c20fa72
Angus Mark <ngsmrk@gmail.com>
2014-03-04 13:19:18 +0000

    removed binding pry

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,7 +12,6 @@ class MessagesController < ApplicationController
     message_params = params['message']
     message = "<html><head><meta charset='utf-8'></head><body><p>#{message_params['icon']}</p><p>#{message_params['title']}</p><p>#{message_params['header']}</p><p>#{message_params['body']}</p></body></html>"
 
-    binding.pry
     little_printer_client = LittlePrinterClient.new(message_params['printer'])
     little_printer_client.message(message)
   end
