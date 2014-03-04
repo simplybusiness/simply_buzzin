bdb97a3c7512b6af416250d142a7c87a540a2d9e
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2014-03-04 14:20:10 +0000

    added styles to the message

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -12,7 +12,7 @@ module MessagesHelper
     options = []
     ["boy","girl","jason"].each do |type|
       (1..7).each do |pos|
-        options << ["#{type} face #{pos}", "#{type}_face_#{pos}"]
+        options << ["#{type} face #{pos}", "#{type}_face_0#{pos}.png"]
       end
     end
     options
--- config/initializers/little_printer.rb
+++ config/initializers/little_printer.rb
@@ -1,2 +1,3 @@
 ENV['PRINTER_CODES'] || raise("No printer codes found. Please set them in ENV['PRINTER_CODES'] separated by ',' (comma)")
+require 'printer_pool'
 PrinterPool.printer_codes = ENV['PRINTER_CODES'].split(",").map(&:strip)
